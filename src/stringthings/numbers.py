c7dae617cb6ff0b77a4fbe389ec58aa95ca59cd4
Martín Araya <martinaraya@gmail.com>
2024-01-27 13:26:31 +0100

    improved `get_number` function to cope with more number representations

--- src/stringthings/numbers.py
+++ src/stringthings/numbers.py
@@ -12,20 +12,43 @@ def is_numeric(string):
     """
     returns True if the string is a number
     """
-    # assert type(string) is str
-    try:
-        float(string)
-        return True
-    except:
-        return False
+    if 'j' in string.lower():
+        try:
+            complex(string)
+            return True
+        except:
+            return False
+    else:
+        string = string.strip('() +-').replace("'", '').replace(' ', '').replace(',', '.')
+        try:
+            float(string)
+            return True
+        except:
+            return False
 
 
 def get_number(string):
     """
-    returns the number, as integer or float, contained in a string
+    returns the number, as integer, float or complex, contained in a numeric string
     """
     if is_numeric(string):
-        try:
-            return int(string)
-        except:
-            return float(string)
+        string = string.lower()
+        if 'j' in string:
+            return complex(string)
+        else:
+            string = string.replace(' ', '').replace("'", '').replace(',', '.').strip('+')
+            if string.endswith('-'):
+                string = '-' + string.strip('-')
+            if string.startswith('(') and string.endswith(')'):
+                string = '-' + string.strip('-()')
+            if '.' in string:
+                if 'e' in string and '.' in string.split('e')[-1]:
+                    return float(string.slit('e')[0]) * 10 ** float(string.split('e')[-1])
+                else:
+                    return float(string)
+            elif 'e' in string:
+                return int(string.split('e')[0]) * 10 ** int(string.split('e')[-1])
+            else:
+                return int(string)
+    else:
+        raise ValueError(f"{string} doesn't represent a number.")
